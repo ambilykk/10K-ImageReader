137aa0578ea77d68606349f91fc6ac4fd437684f
Ambily  K.K <228552@India.TCS.com>
2016-09-23 22:05:47 +0530

    Third Commit

--- Publish/Content/main.js
+++ Publish/Content/main.js
@@ -1,53 +1,68 @@
-﻿var responseContainer;
-var status;
-
-function enableresult() {
-    var resultCard = document.getElementById('resultCard');
-    resultCard.style.display = "block";   
-
-    status = document.getElementById('status');
-
-    responseContainer = document.getElementById('result');
-    responseContainer.innerText = "";
-    var imageURL = document.getElementById('imageURL');
-}
-
-function read() {
-    enableresult();
-    status.innerText = "Extracting Text from Image in progress.....";
-
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            responseContainer.innerText = JSON.parse(request.responseText);            
-            status.innerText = "";
-        }
-    };
-    request.open("get", "/api/ocr?imageURL=" + imageURL.value, true);
-    request.send();      
-}
-
-function readImage()
-{   
-    var uploader = document.getElementById('uploader').files;
-    if (uploader.length > 0) {
-        enableresult();            
-        status.innerText = "Extracting Text from Image in progress.....";
-
-        var formData = new FormData();
-        formData.append("opmlFile", uploader[0]);
+﻿function read() {
+    if (navigator.onLine) {
+        enableresult();
+     
+        var imageURL = document.getElementById('imageURL');
 
         var request = new XMLHttpRequest();
         request.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                responseContainer.innerText = JSON.parse(request.responseText);
-                status.innerText = "";
+                document.getElementById('result').innerText = JSON.parse(request.responseText);
+                document.getElementById("status").textContent = "";
             }
         };
-        request.open("post", "/api/ocr", true);
-        request.send(formData);
+        request.open("get", "/api/ocr?imageURL=" + imageURL.value, true);
+        request.send();
     }
-    else 
-        alert("Select an image");    
+    else
+    {
+        offlinemsg();
+    }
 }
 
+function readImage()
+{
+    if (navigator.onLine) {
+        var uploader = document.getElementById('uploader').files;
+        if (uploader.length > 0) {
+            enableresult();         
+
+            var formData = new FormData();
+            formData.append("opmlFile", uploader[0]);
+
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById('result').innerText = JSON.parse(request.responseText);
+                    document.getElementById("status").textContent = "";
+                }
+            };
+            request.open("post", "/api/ocr", true);
+            request.send(formData);
+        }
+        else
+            alert("Select an image");
+    }
+    else
+    {
+        offlinemsg();
+    }
+}
+
+function enableresult() {
+    var resultCard = document.getElementById('resultCard');
+    resultCard.style.display = "block";
+
+    document.getElementById("status").textContent = "Extracting Text from Image in progress.....";
+    document.getElementById('result').innerText = "";    
+}
+
+function offlinemsg()
+{
+    var resultCard = document.getElementById('resultCard');
+    resultCard.style.display = "block";
+
+    document.getElementById("status").textContent =  "Application is Offline. Please save your pictures and feed to the application when Online.";
+    document.getElementById("read").disabled = true;
+    document.getElementById("upload").disabled = true;
+}
